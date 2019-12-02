d46e8ee2a6b025c85e0de10467d7ad48641fd4d5
theBurnWE <550794050@qq.com>
2019-12-03 00:11:02 +0800

    newceode
    newcode

--- server/src/test/java/leetCode/subjectRang1To50/subject3.java
+++ server/src/test/java/leetCode/subjectRang1To50/subject3.java
@@ -15,7 +15,7 @@ public class subject3 {
 
     private static final Logger logger = LoggerFactory.getLogger(subject3.class);
 
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstringv1(String s) {
         Map<String,String>map = new HashMap();
         int len=0;
         char[] ss =s.toCharArray();
@@ -26,5 +26,23 @@ public class subject3 {
         return len;
 
     }
-
+    public  int lengthOfLongestSubstring03(String s) {
+        int n = s.length(), ans = 0;
+        //创建map窗口,i为左区间，j为右区间，右边界移动
+        Map<Character, Integer> map = new HashMap<>();
+        for (int j = 0, i = 0; j < n; j++) {
+            // 如果窗口中包含当前字符，
+            if (map.containsKey(s.charAt(j))) {
+                //左边界移动到 相同字符的下一个位置和i当前位置中更靠右的位置，这样是为了防止i向左移动
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            //比对当前无重复字段长度和储存的长度，选最大值并替换
+            //j-i+1是因为此时i,j索引仍处于不重复的位置，j还没有向后移动，取的[i,j]长度
+            ans = Math.max(ans, j - i + 1);
+            // 将当前字符为key，下一个索引为value放入map中
+            // value为j+1是为了当出现重复字符时，i直接跳到上个相同字符的下一个位置，if中取值就不用+1了
+            map.put(s.charAt(j), j+1);
+        }
+        return ans;
+    }
 }
